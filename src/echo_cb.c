5282f42345d5a5669265ef87fd6d4a1517b9babc
Kolin63 <theverylastgoodname@gmail.com>
2026-04-08 23:12:25 -0400

    Add echo command for command option testing

---
+++ src/echo_cb.c
@@ -0,0 +1,22 @@
+#include <concord/discord.h>
+#include <string.h>
+
+#include "api.h"
+
+void echo_cb(struct api* api, struct discord* client,
+             const struct discord_interaction* event) {
+  char* content = "(error)";
+
+  for (int i = 0; i < event->data->options->size; ++i) {
+    const char* name = event->data->options->array[i].name;
+    char* value = event->data->options->array[i].value;
+
+    if (strcmp(name, "content") == 0) content = value;
+  }
+
+  struct discord_interaction_response params = {
+      .type = DISCORD_INTERACTION_CHANNEL_MESSAGE_WITH_SOURCE,
+      .data = &(struct discord_interaction_callback_data){.content = content}};
+  api->discord_create_interaction_response(client, event->id, event->token,
+                                           &params, NULL);
+}

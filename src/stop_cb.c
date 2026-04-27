da24001ea9ab7c468546f57bfdf4931ba0d1d0d7
Kolin63 <theverylastgoodname@gmail.com>
2026-04-27 00:08:44 -0400

    Update exit function

--- src/stop_cb.c
+++ src/stop_cb.c
@@ -8,5 +8,5 @@ void stop_cb(const struct api* api, struct discord* client, const struct discord
   api->discord_create_interaction_response(client, event->id, event->token, &params, NULL);
 
   log_info(api, "Stopping bot!");
-  api->bot_exit();
+  api->exit();
 }

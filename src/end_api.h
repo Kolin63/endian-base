af7be1922970b35134e8febb5b4b78f749085918
Kolin63 <theverylastgoodname@gmail.com>
2026-07-09 21:36:21 -0400

    Add comment to API bindings

--- src/end_api.h
+++ src/end_api.h
@@ -155,6 +155,7 @@ int save_write(const char* ns, const char* dir, const char* file, const char* ex
 // dir does not need a trailing slash
 // ext is file extension, and it should not include the dot. for example, a
 // json file has the extension "json", not ".json"
+// out buf should be set to NULL
 int save_read(const char* ns, const char* dir, const char* file, const char* ext, char** out);
 
 // takes a string formatted as namespace:id and replaces the colon with a null
